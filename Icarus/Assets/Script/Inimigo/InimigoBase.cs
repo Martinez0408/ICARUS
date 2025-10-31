1711d5e590a9f953686833ebefd2c24b6d4403b4
Martinez0408 <gabriel.martins.1553461@sga.pucminas.br>
2025-10-31 19:41:57 -0300

    Po não vai

---
+++ Icarus/Assets/Script/Inimigo/InimigoBase.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public abstract class InimigoBase : MonoBehaviour
+{
+   // public abstract void Morrer();
+}
--- Icarus/Assets/Script/Spawn/InimigoSpawnSequence.cs
+++ Icarus/Assets/Script/Spawn/InimigoSpawnSequence.cs
@@ -1,24 +1,58 @@
 using UnityEngine;
+using System.Collections;
+
+
+
 
 public class InimigoSpawnSequence : MonoBehaviour
 {
     
-    [Header("Sequência de inimigos")]
-    [SerializeField] GameObject[] inimigosNaOrdem;
-    [SerializeField] Transform spawnPoint;
-    [SerializeField] float intervaloEntreSpawns = 4f;
 
-   // void Start()
-   // {
-   //     StartCoroutine(SpawnSequencial());
-   // }
+     [System.Serializable]
+    public class EnemySpawnData
+    {
+        public GameObject enemyPrefab;   // Prefab do inimigo
+        public Vector3 position;         // Posição onde vai nascer
+        public float delay;              // Tempo de espera antes do próximo inimigo
+    }
 
-   // IEnumerator SpawnSequencial()
-   // {
-   //     foreach (GameObject inimigo in inimigosNaOrdem)
-   //     {
-   //         Instantiate(inimigo, spawnPoint.position, spawnPoint.rotation);
-   //         yield return new WaitForSeconds(intervaloEntreSpawns);
-   //     }
-   // }
+    [System.Serializable]
+    public class WaveData
+    {
+        public string waveName = "Nova Wave";
+        public EnemySpawnData[] enemies;   // Lista de inimigos dessa wave
+        public float delayAfterWave = 5f;  // Espera antes da próxima wave
+    }
+
+
+    [Header("Sequência de spawn")]
+    public EnemySpawnData[] spawnList;
+
+    [Header("Referência ao GameManager (opcional)")]
+    public GameManager gameManager;
+
+    void Start()
+    {
+        if (gameManager == null)
+            gameManager = GameManager.Mestre;
+
+        StartCoroutine(SpawnSequenceRoutine());
+    }
+
+    IEnumerator SpawnSequenceRoutine()
+    {
+        foreach (var spawn in spawnList)
+        {
+            yield return new WaitForSeconds(spawn.delay);
+
+            if (spawn.enemyPrefab != null)
+            {
+                Instantiate(
+                    spawn.enemyPrefab,
+                    spawn.position,
+                    Quaternion.identity
+                );
+            }
+        }
+    }
 }
